a23c2802d20f3f3ba320f57a82ea2ba5f3aad3da
arbrown7 <144759843+arbrown7@users.noreply.github.com>
2026-02-06 10:44:22 -0700

    Started: Repair the routes page filtering
    Coding Challenge: Emergency Handoff

--- src/controllers/routes/list.js
+++ src/controllers/routes/list.js
@@ -1,9 +1,21 @@
-import { getAllRoutes, getListOfRegions, getListOfSeasons } from '../../models/model.js';
+import { getAllRoutes, getListOfRegions, getListOfSeasons, getRoutesByRegion, getRoutesBySeason } from '../../models/model.js';
 
 export default async (req, res) => {
+    const regionSort = req.query.region || '';
+    const seasonSort = req.query.season || '';
+
     const regions = await getListOfRegions();
     const routes = await getAllRoutes();
     const seasons = await getListOfSeasons();
+    
+    if (regionSort && seasonSort) {
+        //TODO sort by region and season
+
+    } else if (regionSort) {
+        routes = await getRoutesByRegion(regionSort);
+    } else if (seasonSort) {
+        routes = await getRoutesBySeason(seasonSort);
+    }
 
     res.render('routes/list', {
         title: 'Scenic Train Routes',
