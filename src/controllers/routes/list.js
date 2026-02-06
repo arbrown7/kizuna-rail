ce5d390ce3f2b8c44ad5a15e93bd76f50abc21a1
arbrown7 <144759843+arbrown7@users.noreply.github.com>
2026-02-06 09:48:43 -0700

    Reorganize the routes folder structure
    Coding Challenge: Emergency Handoff

--- src/controllers/routes/list.js
+++ src/controllers/routes/list.js
@@ -1,11 +1,11 @@
-import { getAllRoutes, getListOfRegions, getListOfSeasons } from '../models/model.js';
+import { getAllRoutes, getListOfRegions, getListOfSeasons } from '../../models/model.js';
 
 export default async (req, res) => {
     const regions = await getListOfRegions();
     const routes = await getAllRoutes();
     const seasons = await getListOfSeasons();
 
-    res.render('routes/list', { 
+    res.render('routes/list', {
         title: 'Scenic Train Routes',
         regions,
         routes,
